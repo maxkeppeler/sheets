5253f64af98a365aa764e6066c693838b0dd2c2c
Maximilian Keppeler <maximilian.keppeler@gmail.com>
2021-01-03 22:08:35 +0100

    Maintain state

--- input/src/main/java/com/maxkeppeler/bottomsheets/input/InputSheet.kt
+++ input/src/main/java/com/maxkeppeler/bottomsheets/input/InputSheet.kt
@@ -29,6 +29,7 @@ import com.maxkeppeler.bottomsheets.core.layoutmanagers.CustomLinearLayoutManage
 import com.maxkeppeler.bottomsheets.input.databinding.BottomSheetsInputBinding
 import com.maxkeppeler.bottomsheets.input.type.Input
 import com.maxkeppeler.bottomsheets.input.type.InputRadioButtons
+import java.io.Serializable
 
 /** Listener which returns the inputs with the new data. */
 typealias InputListener = (result: Bundle) -> Unit
@@ -39,6 +40,13 @@ typealias InputListener = (result: Bundle) -> Unit
 class InputSheet : BottomSheet() {
 
     override val dialogTag = "InputSheet"
+
+    companion object {
+        private const val STATE_LISTENER = "state_listener"
+        private const val STATE_CONTENT_TEXT = "state_content_text"
+        private const val STATE_INPUT = "state_input"
+        private const val STATE_INPUT_AMOUNT = "state_input_amount"
+    }
 
     private lateinit var binding: BottomSheetsInputBinding
 
@@ -194,6 +202,29 @@ class InputSheet : BottomSheet() {
             input.putValue(bundle, i)
         }
         return bundle
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun onRestoreCustomViewInstanceState(savedState: Bundle?) {
+        savedState?.let { saved ->
+            listener = saved.getSerializable(STATE_LISTENER) as InputListener?
+            contentText = saved.getString(STATE_CONTENT_TEXT)
+            repeat(saved.getInt(STATE_INPUT_AMOUNT)) {
+                val inputItem = saved.getSerializable(STATE_INPUT.plus(it)) as Input
+                input.add(inputItem)
+            }
+        }
+    }
+
+    override fun onSaveCustomViewInstanceState(outState: Bundle) {
+        with(outState) {
+            putSerializable(STATE_LISTENER, listener as Serializable?)
+            putString(STATE_CONTENT_TEXT, contentText)
+            putInt(STATE_INPUT_AMOUNT, input.size)
+            input.forEachIndexed { i, input ->
+                putSerializable(STATE_INPUT.plus(i), input)
+            }
+        }
     }
 
     /** Build [InputSheet] and show it later. */
