4036613b3614e9d8ca7f96f1a05f081ee0f1f8b2
Maximilian Keppeler <maximilian.keppeler@gmail.com>
2021-01-03 22:06:29 +0100

    Maintain state

--- time/src/main/java/com/maxkeppeler/bottomsheets/time_clock/TimeSheet.kt
+++ time/src/main/java/com/maxkeppeler/bottomsheets/time_clock/TimeSheet.kt
@@ -27,6 +27,7 @@ import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import com.maxkeppeler.bottomsheets.core.BottomSheet
 import com.maxkeppeler.bottomsheets.time.databinding.BottomSheetsTimeBinding
+import java.io.Serializable
 
 /** Listener which returns the selected duration time in milliseconds. */
 typealias DurationTimeListener = (timeInSec: Long) -> Unit
@@ -37,6 +38,14 @@ typealias DurationTimeListener = (timeInSec: Long) -> Unit
 class TimeSheet : BottomSheet() {
 
     override val dialogTag = "TimeSheet"
+
+    companion object {
+        private const val STATE_LISTENER = "state_listener"
+        private const val STATE_FORMAT = "state_format"
+        private const val STATE_MIN_TIME = "state_min_time"
+        private const val STATE_MAX_TIME = "state_max_time"
+        private const val STATE_CURRENT_TIME = "state_current_time"
+    }
 
     private lateinit var binding: BottomSheetsTimeBinding
     private lateinit var selector: TimeSelector
@@ -158,6 +167,27 @@ class TimeSheet : BottomSheet() {
             validationListener = validationListener
         )
         currentTime?.let { selector.setTime(it) }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun onRestoreCustomViewInstanceState(savedState: Bundle?) {
+        savedState?.let { saved ->
+            listener = saved.getSerializable(STATE_LISTENER) as DurationTimeListener?
+            format = saved.getSerializable(STATE_FORMAT) as TimeFormat
+            minTime = saved.getLong(STATE_MIN_TIME)
+            maxTime = saved.getLong(STATE_MAX_TIME)
+            currentTime = saved.get(STATE_CURRENT_TIME) as Long?
+        }
+    }
+
+    override fun onSaveCustomViewInstanceState(outState: Bundle) {
+        with(outState) {
+            putSerializable(STATE_LISTENER, listener as Serializable?)
+            putSerializable(STATE_FORMAT, format)
+            putLong(STATE_MIN_TIME, minTime)
+            putLong(STATE_MAX_TIME, maxTime)
+            putLong(STATE_CURRENT_TIME, selector.getTimeInSeconds())
+        }
     }
 
     /** Build [TimeSheet] and show it later. */
