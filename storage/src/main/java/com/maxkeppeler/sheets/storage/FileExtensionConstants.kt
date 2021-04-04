63dff85218381748da9da90c82ad4bd6ec830afd
Maximilian Keppeler <maximilian.keppeler@gmail.com>
2021-04-04 19:47:00 +0200

    Create class to store common file extensions

---
+++ storage/src/main/java/com/maxkeppeler/sheets/storage/FileExtensionConstants.kt
@@ -0,0 +1,193 @@
+/*
+ *  Copyright (C) 2020. Maximilian Keppeler (https://www.maxkeppeler.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+package com.maxkeppeler.sheets.storage
+
+internal const val EXTENSION_PNG = "png"
+internal const val EXTENSION_JPG = "jpg"
+internal const val EXTENSION_JPEG = "jpeg"
+internal const val EXTENSION_JP2 = "jp2"
+internal const val EXTENSION_J2K = "j2k"
+internal const val EXTENSION_JPF = "jpf"
+internal const val EXTENSION_JPX = "jpx"
+internal const val EXTENSION_JPM = "jpm"
+internal const val EXTENSION_MJ2 = "mj2"
+
+internal const val EXTENSION_SVG = "svg"
+internal const val EXTENSION_SVGZ = "svgz"
+internal const val EXTENSION_AI = "ai"
+internal const val EXTENSION_EPS = "eps"
+
+internal const val EXTENSION_PDF = "pdf"
+internal const val EXTENSION_GIF = "gif"
+internal const val EXTENSION_TIFF = "tiff"
+internal const val EXTENSION_TIF = "tif"
+internal const val EXTENSION_PSD = "psd"
+internal const val EXTENSION_GIMP = "xsf"
+internal const val EXTENSION_CORELDRAW = "cdr"
+
+internal const val EXTENSION_RAW = "raw"
+internal const val EXTENSION_CR2 = "cr2"
+internal const val EXTENSION_NEF = "nef"
+internal const val EXTENSION_ORF = "orf"
+internal const val EXTENSION_SR2 = "sr2"
+
+internal const val EXTENSION_BMP = "bmp"
+internal const val EXTENSION_HEIF = "heif"
+internal const val EXTENSION_HEIC = "heic"
+internal const val EXTENSION_INDD = "indd"
+internal const val EXTENSION_IND = "ind"
+internal const val EXTENSION_INDT = "indt"
+internal const val EXTENSION_EXIF = "exif"
+
+
+internal const val EXTENSION_WEBM = "webm"
+internal const val EXTENSION_MPG = "mpg"
+internal const val EXTENSION_MP2 = "mp2"
+internal const val EXTENSION_MPEG = "mpeg"
+internal const val EXTENSION_MPE = "mpe"
+internal const val EXTENSION_MPV = "mpv"
+internal const val EXTENSION_H264 = "h264"
+internal const val EXTENSION_MP4 = "mp4"
+internal const val EXTENSION_M4P = "m4p"
+internal const val EXTENSION_M4V = "m4v"
+internal const val EXTENSION_AVI = "avi"
+internal const val EXTENSION_WMV = "wmv"
+internal const val EXTENSION_MOV = "mov"
+internal const val EXTENSION_QT = "qt"
+internal const val EXTENSION_FLV = "flv"
+internal const val EXTENSION_SWF = "swf"
+
+internal const val EXTENSION_AIF = "aif"
+internal const val EXTENSION_CDA = "cda"
+internal const val EXTENSION_MID = "mid"
+internal const val EXTENSION_MIDI = "midi"
+internal const val EXTENSION_MP3 = "mp3"
+internal const val EXTENSION_MPA = "mpa"
+internal const val EXTENSION_OGG = "ogg"
+internal const val EXTENSION_WAV = "wav"
+internal const val EXTENSION_WMA = "wma"
+internal const val EXTENSION_WPL = "wpl"
+
+
+internal const val EXTENSION_XML = "xml"
+internal const val EXTENSION_DOC = "doc"
+internal const val EXTENSION_DOCX = "docx"
+internal const val EXTENSION_ODT = "odt"
+internal const val EXTENSION_RTF = "rtf"
+internal const val EXTENSION_TEX = "tex"
+internal const val EXTENSION_TXT = "txt"
+internal const val EXTENSION_WPD = "wpd"
+
+internal const val EXTENSION_7Z = "7z"
+internal const val EXTENSION_ARJ = "ARJ"
+internal const val EXTENSION_DEB = "deb"
+internal const val EXTENSION_PKG = "pkg"
+internal const val EXTENSION_RAR = "rar"
+internal const val EXTENSION_RPM = "rpm"
+internal const val EXTENSION_TARGZ = "tar.gz"
+internal const val EXTENSION_Z = "z"
+internal const val EXTENSION_ZIP = "zip"
+
+internal val EXTENSION_IMAGE = mutableListOf(
+    EXTENSION_PNG,
+    EXTENSION_JPG,
+    EXTENSION_JPEG,
+    EXTENSION_JP2,
+    EXTENSION_J2K,
+    EXTENSION_JPF,
+    EXTENSION_JPX,
+    EXTENSION_JPM,
+    EXTENSION_MJ2,
+    EXTENSION_SVG,
+    EXTENSION_SVGZ,
+    EXTENSION_AI,
+    EXTENSION_EPS,
+    EXTENSION_GIF,
+    EXTENSION_TIFF,
+    EXTENSION_TIF,
+    EXTENSION_PSD,
+    EXTENSION_GIMP,
+    EXTENSION_CORELDRAW,
+    EXTENSION_PDF,
+    EXTENSION_RAW,
+    EXTENSION_CR2,
+    EXTENSION_NEF,
+    EXTENSION_ORF,
+    EXTENSION_SR2,
+    EXTENSION_BMP,
+    EXTENSION_HEIF,
+    EXTENSION_HEIC,
+    EXTENSION_INDD,
+    EXTENSION_IND,
+    EXTENSION_INDT,
+    EXTENSION_EXIF,
+)
+
+internal val EXTENSION_VIDEO = mutableListOf(
+    EXTENSION_WEBM,
+    EXTENSION_MPG,
+    EXTENSION_MP2,
+    EXTENSION_MPE,
+    EXTENSION_MPEG,
+    EXTENSION_H264,
+    EXTENSION_MPV,
+    EXTENSION_MP4,
+    EXTENSION_M4P,
+    EXTENSION_M4V,
+    EXTENSION_AVI,
+    EXTENSION_WMV,
+    EXTENSION_MOV,
+    EXTENSION_QT,
+    EXTENSION_FLV,
+    EXTENSION_SWF,
+)
+
+internal val EXTENSION_AUDIO = mutableListOf(
+    EXTENSION_AIF,
+    EXTENSION_CDA,
+    EXTENSION_MID,
+    EXTENSION_MIDI,
+    EXTENSION_MP3,
+    EXTENSION_MPA,
+    EXTENSION_OGG,
+    EXTENSION_WAV,
+    EXTENSION_WMA,
+    EXTENSION_WPL,
+)
+
+internal val EXTENSION_TEXT = mutableListOf(
+    EXTENSION_XML,
+    EXTENSION_DOC,
+    EXTENSION_DOCX,
+    EXTENSION_ODT,
+    EXTENSION_RTF,
+    EXTENSION_TEX,
+    EXTENSION_TXT,
+    EXTENSION_WPD,
+)
+
+internal val EXTENSION_COMPRESSED = mutableListOf(
+    EXTENSION_7Z,
+    EXTENSION_ARJ,
+    EXTENSION_DEB,
+    EXTENSION_PKG,
+    EXTENSION_RAR,
+    EXTENSION_RPM,
+    EXTENSION_TARGZ,
+    EXTENSION_Z,
+    EXTENSION_ZIP,
+)
