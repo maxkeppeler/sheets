8e82c96c6ca3a501c9ddca3ce110641febb21e82
Maximilian Keppeler <maximilian.keppeler@gmail.com>
2022-09-29 23:14:33 +0200

    Remove TextWatcher update delay
    Fixes a problem with the InputEditText when the text is changed and within 300ms the ok button of the dialog is clicked where the resultListener will return the current, but soon to be outdated value.

--- input/src/main/java/com/maxkeppeler/sheets/input/InputSheetExt.kt
+++ input/src/main/java/com/maxkeppeler/sheets/input/InputSheetExt.kt
@@ -16,7 +16,6 @@
 
 package com.maxkeppeler.sheets.input
 
-import android.os.CountDownTimer
 import android.text.Editable
 import android.text.TextWatcher
 import androidx.annotation.RestrictTo
@@ -25,18 +24,11 @@ import androidx.appcompat.widget.AppCompatEditText
 /** Add a [TextWatcher] to the [AppCompatEditText]. */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 fun AppCompatEditText.addTextWatcher(textChanged: ((String) -> Unit)? = null): TextWatcher {
-    var timer: CountDownTimer? = null
     val textWatcher = object : TextWatcher {
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
         override fun afterTextChanged(editable: Editable?) {
-            timer?.cancel()
-            timer = object : CountDownTimer(300, 100) {
-                override fun onTick(millisUntilFinished: Long) = Unit
-                override fun onFinish() {
-                    textChanged?.invoke(editable.toString())
-                }
-            }.start()
+            textChanged?.invoke(editable.toString())
         }
     }
     addTextChangedListener(textWatcher)
